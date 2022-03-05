ea08fdcd9188938d2ee37283dfabb1f2da686d82
caio17harved <caio.croccia@gmail.com>
2022-03-04 22:18:16 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { TabuadaComponent } from './tabuada/tabuada.component';
+
+@NgModule({
+  imports:      [
+     BrowserModule, 
+     FormsModule ,
+     RouterModule.forRoot([
+       {path: ``, component: HomeComponent},
+       {path: `sobre`, component: SobreComponent},
+       {path: `Tabuada`, component: TabuadaComponent}
+     ])
+    ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/tabuada/tabuada.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabuada',
+  templateUrl: './tabuada.component.html',
+  styleUrls: ['./tabuada.component.css']
+})
+export class TabuadaComponent implements OnInit {
+
+  X=5;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
